e110d17b6c2073d69231534f3a0b869d9c8a6df1
sianida26 <chesakeselekkebo@gmail.com>
2021-10-01 20:21:25 +0700

    Done styling login

--- resources/js/RoyalSPBU/pages/guestPages/Login.tsx
+++ resources/js/RoyalSPBU/pages/guestPages/Login.tsx
@@ -8,6 +8,8 @@ import axios from 'axios'
 
 import DB from '../../utils/DB'
 
+import Spinner from '../../components/Spinner'
+
 interface ServerResponse {
     username: string,
     name: string,
@@ -18,12 +20,15 @@ interface ServerResponse {
 export default function Login() {
 
     const {auth, setAuthState} = useAuth()
+    const [isLoading, setLoading] = useState(false)
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [errorMsg, setErrorMsg] = useState('')
 
     let history = useHistory()
 
     const handleLogin = () => {
+        setLoading(true)
         axios({
             method: 'post', 
             data:{username, password}, 
@@ -50,21 +55,107 @@ export default function Login() {
             db.auth.put({key: 'username', value: data.username})
             db.auth.put({key: 'role', value: data.role})
             history.push('/')
+            setErrorMsg('')
         })
         .catch(error => {
-            console.error('error!')
-            console.log(error.response)
+            //Any status codes that falls outside the range of 2xx cause this function to trigger
+            //Do something with response error
+            let pesan = "error"
+            if (error.code === "ECONNABORTED"){ //Handle timeout error
+                pesan = "Ups. Jaringan Anda terganggu. Silakan coba lagi";
+            } else if (error.code) { //Handle other request error
+                pesan = `Ups. Terjadi error. code: ${error.code}`;
+            } else { //handle response error
+                let errorCode = error.response.status
+                switch(errorCode){
+                    case 400 : pesan = "Terjadi error saat mengirim (400)"; break;
+                    case 403 : pesan = "Anda tidak memiliki akses untuk ini"; break;
+                    case 404 : pesan = "URL / sesuatu yang dicari tidak ada"; break;
+                    case 405 : pesan = "Metode request ini tidak diizinkan";  break;
+                    case 408 : pesan = "Ups. Jaringan Anda terganggu. Silakan coba lagi"; break;
+                    case 419 : pesan = "Token CSRF Anda hilang. silakan tutup browser kemudian coba kembali"; break;
+                    case 422 : pesan = "Username atau Password anda salah"; break;
+                    case 429 : pesan = "Anda terlalu banyak melakukan request ini"; break;
+                    case 500: //server error
+                        pesan =`Ups. Terjadi error di dalam server. silakan coba lagi nanti (${errorCode})`;
+                        break; 
+                    default: pesan = `Ups. terjadi error (${errorCode})`;
+                }
+            }
+            setErrorMsg(pesan)
+            setLoading(false)
         })
-        history.replace('/')
+        // history.replace('/')
     }
 
     return (
-        <div className="tw-flex tw-flex-col">
-            <span className="tw-border tw-border-black">Username</span>
+        <div className="tw-flex tw-flex-col tw-w-screen tw-h-screen tw-justify-center tw-items-center tw-px-4">
+            <div 
+                className="tw-px-8 tw-py-8 tw-w-full tw-rounded-xl tw-border tw-border-gray-300"
+                style={{maxWidth: '560px'}}
+            >
+
+                {/* logo */}
+                <div className="tw-flex tw-gap-3 tw-justify-center">
+                    <img src="/images/logos/logo.svg" alt="Logo Royal SPBU" />
+                </div>
+
+                <hr className="border-gray-100 tw-my-4" />
+
+                {/* title */}
+                <p className="tw-font-light tw-text-2xl tw-text-gray-800 tw-text-center">Masuk</p>
+
+                {
+                    errorMsg && <p className="tw-mt-4 tw-text-white tw-text-center tw-bg-red-500 tw-py-1 tw-px-2 tw-rounded-md">{errorMsg}</p>
+                }
+
+                <div className={`tw-w-full tw-px-4 tw-flex tw-flex-col tw-gap-6 tw-mt-4 ${isLoading && 'tw-opacity-75'}`}>
+
+                    {/* username */}
+                    <input 
+                        className="tw-text-md tw-w-full tw-max-w-screen-sm tw-border tw-rounded-md tw-border-gray-300 tw-py-3 tw-px-4 tw-placeholder-black" 
+                        placeholder="Username"
+                        autoComplete="username"
+                        disabled={isLoading}
+                        value={username}
+                        onChange={(e) => {setUsername(e.target.value)}}
+                    />
+
+                    {/* password */}
+                    <input
+                        autoFocus
+                        className="tw-text-md tw-w-full tw-max-w-screen-sm tw-border tw-rounded-md tw-border-gray-300 tw-py-3 tw-px-4 tw-placeholder-black" 
+                        placeholder="Password"
+                        type="password"
+                        autoComplete="currentPassword"
+                        value={password} 
+                        disabled={isLoading}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    {/* login button */}
+                    <div className="tw-flex tw-justify-end tw-mt-2">
+                        <button 
+                            className="tw-bg-primary-600 tw-text-white tw-justify-self-end tw-py-2 tw-px-3 tw-rounded-md"
+                            onClick={handleLogin}
+                            disabled={isLoading}
+                        >
+                            {
+                                isLoading ? <span className="tw-flex tw-gap-2 tw-items-center">
+                                    <Spinner /> Loading ...
+                                </span>
+                                : 'Masuk'
+                            }
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <span className="tw-mt-2 tw-text-xs">&copy; 2021 Royal SPBU</span>
+            {/* <span className="tw-border tw-border-black">Username</span>
             <input onChange={(e) => {setUsername(e.target.value)}} value={username} />
             <span>Password</span>
-            <input type="password" className="tw-border tw-border-black" value={password} onChange={e => setPassword(e.target.value)} />
-            <button className="" onClick={handleLogin}>Masuk</button>
+            <input type="password" className="tw-border tw-border-black"  />
+            <button className="" >Masuk</button> */}
         </div>
     )
 }
